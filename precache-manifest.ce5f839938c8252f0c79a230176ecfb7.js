0adc637804254c49d56c35c7a5e44d9b5c5a8edf
Carlos Domínguez García <carlosdg97@gmail.com>
2019-09-11 11:21:24 +0100

    Updates

---
+++ precache-manifest.ce5f839938c8252f0c79a230176ecfb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc4e10b0097b9af5a687752abade0c6",
+    "url": "/yatla/index.html"
+  },
+  {
+    "revision": "82b790338616606b6d66",
+    "url": "/yatla/static/css/main.42bdf525.chunk.css"
+  },
+  {
+    "revision": "798c64f921111fd739b7",
+    "url": "/yatla/static/js/2.c70fe4ea.chunk.js"
+  },
+  {
+    "revision": "82b790338616606b6d66",
+    "url": "/yatla/static/js/main.91be0414.chunk.js"
+  },
+  {
+    "revision": "972b0b01ab4e8c888832",
+    "url": "/yatla/static/js/runtime~main.a756d1c6.js"
+  }
+]);
